e508e0f56731d7899989cd438714f3b0d3b86cf2
Felix Nunez <felix.nunez@vass.com.mx>
2025-01-06 15:19:26 -0700

    Logging

--- src/main/java/com/vass/universidad/controllers/PrimerRestController.java
+++ src/main/java/com/vass/universidad/controllers/PrimerRestController.java
@@ -1,18 +1,31 @@
 package com.vass.universidad.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/restapi")
 public class PrimerRestController {
 
-    @GetMapping("/hola-mundo")
-    public ResponseEntity<String> holaMundo(){
-        return new ResponseEntity<>("Hola Mundo! =D", HttpStatus.ACCEPTED);
-    }
+    Logger logger = LoggerFactory.getLogger(PrimerRestController.class);
 
+    @GetMapping("/hola-mundo")
+    public ResponseEntity<Map<String, String>> holaMundo(){
+        Map<String, String> mensaje = new HashMap<>();
+        mensaje.put("saludo", "Hola Mundo! =D");
+        logger.trace("trace log");
+        logger.debug("debug log");
+        logger.info("info log");
+        logger.warn("warning log");
+        logger.error("error log");
+        return new ResponseEntity<>(mensaje, HttpStatus.ACCEPTED);
+    }
 }
